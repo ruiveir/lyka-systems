5c2600e24c339a1d5cebb1cbfee723492921d71f
nill546@hotmail.com <Edgar>
2020-08-24 16:46:01 +0100

    botao eliminar notificacoes

--- routes/web.php
+++ routes/web.php
@@ -253,3 +253,8 @@ Route::put('/restaurar-password/{user}/nova-password', 'AccountConfirmationContr
 
 /* Listagens */
 Route::resource('/listagens', 'ListagemController');
+
+
+
+/* Para Eliminar */
+Route::get('/data', 'EdgarTesteController@index');
