6ab6f85ed15ddc777483bfb5ff5ba07447fb5771
José Areia <jose.apareia@gmail.com>
2021-01-05 17:37:10 +0000

    Atualização na dashboard
    Substituição de um dos cartões da dashboard de forma a ocupar o espaço pretendido para a presença dos pagamentos pendentes

--- resources/views/dashboard/partials/admin.blade.php
+++ resources/views/dashboard/partials/admin.blade.php
@@ -87,38 +87,18 @@
     <div class="row">
         <!-- Content Column -->
         <div class="col-lg-6 mb-4">
-            <!-- Project Card Example -->
+            <!-- Approach -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Pagamentos pendentes (por mês)</h6>
                 </div>
                 <div class="card-body">
-                    <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-                    <div class="progress mb-4">
-                        <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-                    <div class="progress">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
+                    <p class="mb-0 text-center">Não existem pagamentos pendentes este mês.</p>
                 </div>
             </div>
         </div>
 
         <div class="col-lg-6 mb-4">
-
             <!-- Approach -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
@@ -170,7 +150,6 @@
                     @endif
                 </div>
             </div>
-
         </div>
     </div>
 
